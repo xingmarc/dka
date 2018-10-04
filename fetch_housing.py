11ee133b1c544b3b2f71d28a0dec2f8bb46148c3
Xing <xingmarc@yeah.net>
2018-10-03 23:36:29 -0700

    add new file.

---
+++ fetch_housing.py
@@ -0,0 +1,23 @@
+import os
+import tarfile
+from six.moves import urllib
+import pandas as pd
+
+DOWNLOAD_ROOT = "http://raw.githubusercontent.com/ageron/handson-ml/master/"
+HOUSING_PATH = "datasets/housing"
+
+HOUSING_URL = DOWNLOAD_ROOT + HOUSING_PATH + "/housing.tgz"
+
+def fetch_housing_data(housing_url=HOUSING_URL, housing_path=HOUSING_PATH):
+    if not os.path.isdir(housing_path):
+        os.makedirs(housing_path)
+    tgz_path = os.path.join(housing_path, "housing.tgz")
+    urllib.request.urlretrieve(housing_url, tgz_path)
+    housing_tgz = tarfile.open(tgz_path)
+    housing_tgz.extractall(path=housing_path)
+    housing_tgz.close()
+
+def load_housing_data(housing_path=HOUSING_PATH):
+    csv_path = os.path.join(housing_path, "housing.csv")
+    return pd.read_csv(csv_path)
+
